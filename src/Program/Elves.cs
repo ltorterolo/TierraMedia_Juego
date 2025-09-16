70c8720cbc22f4e8ba18743d3e4aa114485d3fdc
ltorterolo <luca.torterolo@correo.ucu.edu.uy>
2025-09-16 16:24:05 -0300

    Idea de clase

--- src/Program/Elves.cs
+++ src/Program/Elves.cs
@@ -1,14 +1,27 @@
-namespace Program;
+namespace RoleplayGame;
 
 public class Elves
 {
-    public string Name { get; }
-    public int Life { get; }
-    private int Magic { get; }
-    public Elves (string nombre, int vida, int magia)
+    public string Name { get; set; }
+    public int Life { get; set; }
+    private int Magic { get; set; }
+
+    public List<item> Elements { get; set; }
+    public Elves (string nombre, int vida, int magia, List<item> inventario)
     {
         Name = nombre;
         Life = vida;
         Magic = magia;
+        Elements = inventario;
+    }
+
+    public void AddElemento(Item item)
+    {
+        inventario.Add(item);
+    }
+
+    public void SubsElemento(Item item)
+    {
+        inventario.Pop(item);
     }
 }
