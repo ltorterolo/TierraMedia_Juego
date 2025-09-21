7814cadaf2899ba9106333542b7ee39413269edb
franciscamartin <francisca.martin@correo.ucu.edu.uy>
2025-09-21 16:25:05 -0300

    arreglos finales

--- src/Program/Dwarves.cs
+++ src/Program/Dwarves.cs
@@ -1,27 +1,28 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace Program;
+﻿namespace Program;
 
 public class Dwarve
 {
     public string Name { get; }
 
-    private static int MaxLife = 150;
+    private static int MaxLife = 120;
 
+    // Ataque y defensa base 
     private int Attack = 40;
-
     private int Defense = 75;
     
     public int Life = MaxLife;
 
+    // lista de items que tiene el enano
     private List<Item> Items = new List<Item>();
 
+    // Constructor de Enano
     public  Dwarve (string name)
     {
         Name = name;
     }
-
-    public int getAttack()
+    
+    // Calcula los puntos de ataque, sumando los valores de ataque de los items que tiene el personaje
+    public int GetAttack()
     {
         foreach (Item objeto in Items)
         {
@@ -31,7 +32,8 @@ public class Dwarve
         return Attack;
     }
 
-    public int getDefense()
+    // Calcula los puntos de defensa, sumando los valores de defensa de los items que tiene el personaje
+    public int GetDefense()
     {
         foreach (Item objeto in Items)
         {
@@ -41,24 +43,33 @@ public class Dwarve
         return Defense;
     }
 
-    public int RecieveAttack(int ataque)
+    // Recibe un ataque y ajusta la vida
+    public int ReceiveAttack(int ataque)
     {
-        return Life -= ataque + getDefense();
+        return Life -= ataque - GetDefense();
     }
 
+    // Cura al enano sumando el "Healing" de cada ítem
     public void Heal()
     {
         foreach (Item objeto in Items)
         {
             Life += objeto.Healing;
         }
+        // Tope por si se pasa de la vida máxima
+        if (Life > MaxLife)
+        {
+            Life = MaxLife;
+        }
     }
 
+    // Agrega un ítem a la lista de elementos
     public void AddItem(Item objeto)
     {
         Items.Add(objeto);
     }
 
+    // Saca un ítem de la lista de elementos
     public void RemoveItem(Item objeto)
     {
         Items.Remove(objeto);
