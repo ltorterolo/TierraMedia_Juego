1d88cbd8736eef04c8965cd5a5895dc82c8b4a69
franciscamartin <francisca.martin@correo.ucu.edu.uy>
2025-09-21 12:45:44 -0300

    clase enanos

---
+++ src/Program/Dwarves.cs
@@ -0,0 +1,55 @@
+﻿namespace RoleplayGame;
+
+public class Dwarves
+{
+    public string name { get; }
+    private int MaxLife = 150;
+    private int Life = 150;
+    
+    private int Attack = 50;
+    private int Defense = 75;
+    
+    private List<Item> Items = new List<Item>();
+    
+    public int getAttack()
+    {
+        foreach (Item objeto in Items)
+        {
+            Attack += Item.Attack();
+        }
+
+        return Attack;
+    }
+    public int getDefense()
+    {
+        foreach (Item objeto in Items)
+        {
+            Defense += Item.Defense();
+        }
+
+        return Defense;
+    }
+
+    public int reciveAttack(int ataque)
+    {
+        return Life -= ataque + getDefense();
+    }
+
+    public void heal()
+    {
+        Life += MaxLife / 2;
+        foreach (Item objeto in Items)
+        {
+            Life += Item.Healing();
+        }
+    }
+    public void addItem(Item objeto)
+    {
+        Items.Add(objeto);
+    }
+
+    public void removeItem(Item objeto)
+    {
+        Items.Remove(objeto);
+    }
+}
